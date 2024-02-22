e0e007918add7aa3b0f9a58292ca868baf309d08
jonlauvstad <jonalauvstad@hotmail.com>
2024-02-22 12:58:39 +0100

    anders22feb 1257 api_venue i add_lecture_one.html js mm

---
+++ GeoFlask/utility/venue.py
@@ -0,0 +1,16 @@
+class Venue:
+    def __init__(self, id, name, description, locationId, streetAddress, postCode, city, capacity, locationName, link, links):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.locationId = locationId
+        self.streetAddress = streetAddress
+        self.postCode = postCode
+        self.city = city
+        self.capacity = capacity
+        self.locationName = locationName
+        self.link = link
+        self.links = links
+
+
+
